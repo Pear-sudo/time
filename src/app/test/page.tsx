8d2be1e394a00632456d7383a4edae6a3db1f461
adam <adameve1981zero@gmail.com>
2023-12-04 13:30:27 +0800

    skeleton of windowManager.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,11 @@
 import {ColorList} from "@/app/elements/colorList";
 import {JSX} from "react";
+import {WindowManager} from "@/app/utility/windowManager";
 
 export default function Test(): JSX.Element {
+    const windowManager = new WindowManager()
+    const controller = windowManager.createWindow({view: <div></div>})
+    controller.closeWindow()
     return (
         <ColorList/>
     )
--- src/app/utility/windowManager.tsx
+++ src/app/utility/windowManager.tsx
@@ -1,11 +1,97 @@
-import {JSX} from "react";
+import React, {JSX} from "react";
 
-class WindowManager {
-    createWindow(view: JSX.Element): void {
+export class WindowManager {
+    static get ins(): WindowManager {
+        return this._ins;
+    }
+    private static _ins: WindowManager
+
+    private readonly baseZ: number = 1000
+
+    private readonly vMap: Map<number, View> = new Map()
+
+    constructor() {
+        if (!WindowManager._ins) {
+            WindowManager._ins = this
+        }
+        return WindowManager._ins
+    }
+
+    /**
+     * @returns It's the caller's responsibility to pass this controller to view, if necessary
+     * */
+    createWindow(op: CreateWindowOp): WindowController {
+        const view = new View(op.view)
+        const handle = view.cTime.valueOf()
+        this.vMap.set(handle, view)
+        return new WindowController(handle)
+    }
+
+    closeWindows(...controllers: WindowController[]) {
+        for (const controller of controllers) {
+            this.closeWindow(controller)
+        }
+    }
+
+    closeWindow(controller: WindowController) {
+        const handle = controller.handle
+        this.closeW(handle)
+    }
+
+    private closeW(handle: number) {
 
     }
 
-    closeAll() {
+    private closeAll() {
 
+    }
+
+    private generateWindows(): JSX.Element[] {
+        const ws: JSX.Element[] = []
+        return ws
+    }
+
+    initContext(): JSX.Element {
+        return (
+            <div style={{zIndex: this.baseZ}}>
+                {this.generateWindows()}
+            </div>
+        )
+    }
+}
+
+export interface CreateWindowOp {
+    view: JSX.Element,
+    priority?: number
+}
+
+class View {
+    private readonly view: JSX.Element
+    private readonly _cTime: Date
+
+    constructor(view: React.JSX.Element) {
+        this.view = view;
+        this._cTime = new Date()
+    }
+
+    get cTime(): Date {
+        return this._cTime;
+    }
+}
+
+class WindowController {
+    private readonly _handle: number
+    private readonly windowManager: WindowManager = new WindowManager()
+
+    get handle(): number {
+        return this._handle;
+    }
+
+    constructor(handle: number) {
+        this._handle = handle;
+    }
+
+    closeWindow(): void {
+        this.windowManager.closeWindow(this)
     }
 }
