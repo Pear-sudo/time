5d7e8576305fe40bab90c2b810f56063804487c0
adam <adameve1981zero@gmail.com>
2023-12-29 17:41:54 +0800

    bug fix: currentTimeLine should be on top of other events

--- src/app/elements/currentTimeLine.tsx
+++ src/app/elements/currentTimeLine.tsx
@@ -2,7 +2,7 @@ import React, {JSX} from "react";
 
 export function CurrentTimeLine(): JSX.Element {
     return (
-        <div className={'relative w-full bg-blue-600'} style={{height: '0.1rem'}}>
+        <div className={'relative w-full bg-blue-600 z-10'} style={{height: '0.1rem'}}>
             <div className={'absolute rounded-full bg-blue-600 left-0 top-1/2  -translate-x-3/4'}
                  style={{width: '0.5rem', height: '0.5rem', transform: 'translate(-50%, -50%)'}}>
             </div>
