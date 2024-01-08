c0d0892ad4eb2d191bc4a4c39b91e5ba09f016a6
adam <adameve1981zero@gmail.com>
2024-01-08 22:25:46 +0800

    pass data back using the new observer

--- src/app/elements/inputs/advancedDateSelector.tsx
+++ src/app/elements/inputs/advancedDateSelector.tsx
@@ -7,12 +7,17 @@ import {DataWrapper, StateClass} from "@/app/elements/inputs/helper/inputHelper"
 
 export function AdvancedDateSelector(prop: { parentData?: DataWrapper<Date> }): JSX.Element {
     const [selectedDate, setSelectedDate] = useState(new Date())
+    function didSet(newValue: Date, oldValue: Date) {
+        if (prop.parentData) {
+            prop.parentData.setData(newValue)
+        }
+    }
     return (
         <div>
             <DateDisplay date={selectedDate}/>
             <MonthlyCalendar anchor={selectedDate} allowYearSelection={true}
-                             parentData={new StateClass(selectedDate, setSelectedDate)}/>
-            <YearSelector selectedYear={selectedDate} parentData={new StateClass(selectedDate, setSelectedDate)}/>
+                             parentData={new StateClass(selectedDate, setSelectedDate, undefined, didSet)}/>
+            <YearSelector selectedYear={selectedDate} parentData={new StateClass(selectedDate, setSelectedDate, undefined, didSet)}/>
         </div>
     )
 }
