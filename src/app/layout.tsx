7eeafd40fb3d9de60188341d495ec2593ea8358d
adam <adameve1981zero@gmail.com>
2024-01-17 14:35:53 +0800

    add hint for javascript
    in case the user does not enable javascript and complains that this site does not work at all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: 'A Time Manager',
-  description: 'Help you to manage your time.',
+    title: 'A Time Manager',
+    description: 'Help you to manage your time.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+export default function RootLayout({children,}: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en" className={'overflow-hidden'}>
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en" className={'overflow-hidden'}>
+        <body className={inter.className}>
+        <div>Please enable javascript</div>
+        {children}
+        </body>
+        </html>
+    )
 }
