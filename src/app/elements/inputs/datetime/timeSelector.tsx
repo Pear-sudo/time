8c894690635ca32dfc0c8a19c71d0f323f31bea3
adam <adameve1981zero@gmail.com>
2024-01-25 21:44:08 +0800

    double circled clock face

--- src/app/elements/inputs/datetime/timeSelector.tsx
+++ src/app/elements/inputs/datetime/timeSelector.tsx
@@ -1,22 +1,35 @@
 import {JSX} from "react";
+import {Theme} from "@/app/theme";
 
 export function TimeSelector(prop: {}): JSX.Element {
-    const numbers = [...Array(12).keys()].map(n => {
-        const rotation = 30 * n
-        return (
-            <div style={{
-                transform: `rotate(${rotation}deg) translate(1px, -100px) rotate(-${rotation}deg)`,
-                transformOrigin: '0 0'
-            }} key={n} className={'absolute'}>
-                {n}
-            </div>
-        )
+    const outerNumbers = circledElements({numbers: [...Array(12).keys()]})
+    const innerNumbers = circledElements({
+        numbers: [...Array.from({length: 12},
+            (_, i) => i + 12)],
+        radius: 70
     })
     return (
         <div>
             <div className={'rounded-full bg-gray-300 w-52 h-52 flex justify-center items-center relative'}>
-                {numbers}
+                {outerNumbers}
+                {innerNumbers}
             </div>
         </div>
     )
+}
+
+function circledElements(op: { numbers: number[], radius?: number }) {
+    op.radius = op.radius ?? 100
+
+    return op.numbers.map((n, index) => {
+        const rotation = 30 * index
+        return (
+            <div style={{
+                transform: `rotate(${rotation}deg) translate(1px, -${op.radius}px) rotate(-${rotation}deg)`,
+                transformOrigin: '0 0'
+            }} key={n} className={`absolute ${Theme.button}`}>
+                {n}
+            </div>
+        )
+    })
 }
