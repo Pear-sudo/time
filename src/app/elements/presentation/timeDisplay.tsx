8cc04b57b7fb0f93feb7c93ffd56facf7f6f1f68
adam <adameve1981zero@gmail.com>
2024-02-07 20:49:29 +0800

    update the format of hour display

--- src/app/elements/presentation/timeDisplay.tsx
+++ src/app/elements/presentation/timeDisplay.tsx
@@ -1,6 +1,6 @@
 export function TimeDisplay(prop: { date: Date }) {
     const date = prop.date
-    const hourStr = new Intl.DateTimeFormat("en-US", {hour: '2-digit'}).format(date)
+    const hourStr = new Intl.DateTimeFormat("en-US", {hour: '2-digit', hourCycle: 'h23'}).format(date)
     const minuteStr = new Intl.DateTimeFormat("en-US", {minute: '2-digit'}).format(date)
 
     const formattedDate: string = `${hourStr}:${minuteStr}`
