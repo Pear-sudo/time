537ac4853935a68239fa7562f32b368e5605ab00
adam <adameve1981zero@gmail.com>
2024-02-08 17:58:20 +0800

    add some special keys to check for

--- src/app/utility/KeyboardManager.ts
+++ src/app/utility/KeyboardManager.ts
@@ -13,14 +13,26 @@ export class KeyboardManager {
         }
         console.log('Down: ' + e.key)
         this.activeKeys.add(e.key)
+        this.checkKeysAfterDown()
     }
 
     keyUp(e: React.KeyboardEvent): void {
         console.log('Up: ' + e.key)
         this.activeKeys.delete(e.key)
+        this.checkKeysAfterUp()
     }
 
     registerKeys(keys: string[]) {
+
+    }
+
+    // We must distinguish these two types of checking. They are different, though after up checking shall not be used
+    // in most cases.
+    private checkKeysAfterDown() {
+        this.checkKeys()
+    }
+
+    private checkKeysAfterUp() {
 
     }
 
@@ -28,3 +40,19 @@ export class KeyboardManager {
 
     }
 }
+
+export type SpecialKey =
+    'Control'
+    | 'Shift'
+    | 'Alt'
+    | 'Enter'
+    | 'Escape'
+    | 'Meta'
+    | 'Backspace'
+    | 'Delete'
+    | 'CapsLock'
+    | 'Tab'
+    | 'ArrowUp'
+    | 'ArrowDown'
+    | 'ArrowLeft'
+    | 'ArrowRight'
